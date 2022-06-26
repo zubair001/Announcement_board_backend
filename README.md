222a279624e79e1699c1eb2fc153471708e2c521
zubair001 <zubairtub@gmail.com>
2022-06-26 18:22:21 +0200

    Readme updated

---
+++ README.md
@@ -0,0 +1,15 @@
+This is the backend part of Announcement Board application.
+## Available Scripts
+In the project directory, you can run it using:
+### `npm start`
+Backend will run in PORT=3001 (http://localhost:3001).
+
+
+## Technologies
+The following technologies have been used:  
+- ExpressJS
+## Storage
+According to the instruction no database is used, instead the data have been stored locally.
+Initially, 20 random announcement posts have been generated in random dates.   
+
+   
--- data/dataStore.js
+++ data/dataStore.js
@@ -7,12 +7,24 @@ exports.DataStore = class DataStore {
         this.loadData();
     }
 
+    randomPost = [
+        "Hi, I found a key, Kate", 
+        "Anyone has a plan for the weekend? Thanks! Boby",
+        "I lost my key in the park. If anyone find it, please let me know. Helen",
+        "I found a set of keys in the park. Anyone missing please contact. Thanks! Billie",
+        "Anyone has any extra ticket for Coldplay concert? Thanks! Sami",
+        "I am going to join a BBQ party this evening. Who is isterested to join? Best! Charlie"];
+
+    getRandomPost(){
+        return (this.randomPost[Math.floor(Math.random() * 5)]);
+    }
+
     loadData() {
         let index = 0;
         while ( index < 20 ){
             index++;
             this.data.set(crypto.randomUUID(), {
-                "post": "Random announcement generated",
+                "post": this.getRandomPost(),
                 "date": utils.getRandomDate(new Date(2022, 4, 15), new Date(), 0, 24)
             });
         }
